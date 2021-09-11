4fde594244a41495baf13e331689fb990372fe7c
Mahmud Youssef <mahmud.youssef@icloud.com>
2021-09-11 11:00:59 +0200

    setup jaeger tracing

--- reference-app/backend/app.py
+++ reference-app/backend/app.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template, request, jsonify
+import logging
 
 import pymongo
-import logging
+from flask import Flask, jsonify, render_template, request
+from flask_opentracing import FlaskTracing
 from flask_pymongo import PyMongo
+from jaeger_client import Config
 from prometheus_flask_exporter import PrometheusMetrics
 
 logging.basicConfig(level=logging.INFO)
@@ -16,6 +18,22 @@ app.config['MONGO_DBNAME'] = 'example-mongodb'
 app.config['MONGO_URI'] = 'mongodb://example-mongodb-svc.default.svc.cluster.local:27017/example-mongodb'
 
 mongo = PyMongo(app)
+
+
+config = Config(
+    config={
+        'sampler': {
+            'type': 'const',
+            'param': 1
+        },
+        'logging': True,
+        'reporter_batch_size': 1,
+    },
+    service_name="backend-service"
+)
+
+jaeger_tracer = config.initialize_tracer()
+tracing = FlaskTracing(jaeger_tracer, True, app)
 
 @app.route('/')
 def homepage():
